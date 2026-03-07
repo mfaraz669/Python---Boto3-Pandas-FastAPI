acdc07c1de130e083e50414c1878d4e38d16a51b
mfaraz669 <mfaraz669@yahoo.com>
2026-03-07 15:24:44 +0530

    2nd push

---
+++ Lists-Tuple/lists.py
@@ -0,0 +1,13 @@
+marks = [23, 45, 67, 89, 90]
+print(marks[1])
+print(type(marks))
+
+student = ["John", 21, "Computer Science", 87.5, True]
+student[0] = "Jane"
+print(type(student[4]))
+print(student)
+student.append("A+") # adds "A+" to the end of the list
+print(student)
+print(student[0:3]) # prints the first three elements of the list
+print(student[-2:]) # prints the last two elements of the list
+
