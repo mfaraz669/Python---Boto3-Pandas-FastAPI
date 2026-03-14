99bdcdd9a8ac480887978a1644a25f569086ba60
mfaraz669 <mfaraz669@yahoo.com>
2026-03-14 23:40:16 +0530

    latest push

---
+++ Loops/loops.py
@@ -0,0 +1,16 @@
+#loops
+# for i in range(15):
+#     print(i)
+
+for i in range(5, 15):
+    print(i)
+
+# while True:
+#     print("This is an infinite loop")
+
+count = 100
+while count <= 115:
+    print(count)
+    count += 1
+    print(count)
+
