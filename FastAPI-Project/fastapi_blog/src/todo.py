236d95f15856ea0fa589ef45d0aacf2c42a82e07
Mohammed Faraz <mfaraz669@yahoo.com>
2026-09-24 12:06:57 +0530

    latest

--- FastAPI-Project/fastapi_blog/src/todo.py
+++ FastAPI-Project/fastapi_blog/src/todo.py
@@ -18,3 +18,32 @@ def create_todo(todo: Todo):
 def get_todo():
     return todos
 
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+    return {"error": "Todo not found"}
+
+#put api
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, updated_todo:Todo):
+    for index, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos[index] = updated_todo
+            return {
+                "message:" : "Data Updated",
+                "data": updated_todo
+            }
+    return {
+        "message": "error todo not found"
+    }
+
+#delete api
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for index, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos.pop(index)
+            return {"message:" : "Data Deleted"}
+    return {"error": "todo not found"}
